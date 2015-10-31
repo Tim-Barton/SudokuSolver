ed5271599f2bd8ba3249f9d61536f825d2e3df63
Tim Barton <Tim-Barton@users.noreply.github.com>
2015-10-31 15:16:06 +1000

    Adding the start of the gui (completely blank) and the eclipse files for ease

---
+++ src/gui.py
@@ -0,0 +1,19 @@
+'''
+Created on 4 Jul 2015
+
+@author: sphinx
+'''
+
+import tkinter as tk
+
+class Gui(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
